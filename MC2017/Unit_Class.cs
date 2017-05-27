0e60fe71fd1265f10bfdb804acd164c3eb53b847
HanChangHee <jaduwvad2@gmail.com>
2017-05-27 14:10:35 +0900

    Unit_Class 작업중

--- MC2017/Unit_Class.cs
+++ MC2017/Unit_Class.cs
@@ -4,30 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace wpfTest
+namespace MC2017
 {
     class Unit_Class
     {
-        private String name;
+        public enum class_Type
+        {
+            CLASS,
+            INTERFACE,
+            ABSTRACT,
+            UTILITY,
+            ENUMERATION
+        }
+
+        private string name { get; set; }
+        private class_Type type { get; set; }
+
+        private List<Unit_Value> val;
+        private List<Unit_Method> method;
         public List<Unit_Line> from;
         public List<Unit_Line> to;
 
-        public Unit_Class(String name)
+        public Unit_Class(String name, class_Type type)
         {
-
+            this.name = name;
+            this.type = type;
         }
 
         public void delete_Class()
         {
 
         }
-        public void add_Line_From()
+        public void add_Line_From(Unit_Line line_From)
         {
-
+            from.Add(line_From);
         }
-        public void add_Line_To()
+        public void add_Line_To(Unit_Line line_To)
         {
-
+            to.Add(line_To);
+        }
+        public void add_Unit_Value(Unit_Value val)
+        {
+            this.val.Add(val);
+        }
+        public void add_Unit_Method(Unit_Method method)
+        {
+            
         }
     }
 }
--- MC2017/Unit_Line.cs
+++ MC2017/Unit_Line.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace wpfTest
+namespace MC2017
 {
     class Unit_Line
     {
