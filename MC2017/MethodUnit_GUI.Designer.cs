f5b52502ee99ab6a1d744ef2cd900a7a729616e4
HanChangHee <jaduwvad2@gmail.com>
2017-05-27 22:50:43 +0900

    methodUnit_GUI 생성

--- MC2017/MethodUnit_GUI.Designer.cs
+++ MC2017/MethodUnit_GUI.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MC2017
 {
-    partial class Value_Unit_GUI
+    partial class MethodUnit_GUI
     {
         /// <summary> 
         /// 필수 디자이너 변수입니다.
@@ -29,79 +29,92 @@
         private void InitializeComponent()
         {
             this.accessModifier = new System.Windows.Forms.ComboBox();
-            this.checkStatic = new System.Windows.Forms.RadioButton();
-            this.checkFinal = new System.Windows.Forms.RadioButton();
             this.type = new System.Windows.Forms.TextBox();
             this.name = new System.Windows.Forms.TextBox();
+            this.checkStatic = new System.Windows.Forms.RadioButton();
+            this.checkAbstract = new System.Windows.Forms.RadioButton();
             this.create = new System.Windows.Forms.Button();
+            this.param = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // accessModifier
             // 
             this.accessModifier.FormattingEnabled = true;
+            this.accessModifier.Items.AddRange(new object[] {
+            "public",
+            "private",
+            "protected"});
             this.accessModifier.Location = new System.Drawing.Point(0, 0);
             this.accessModifier.Name = "accessModifier";
             this.accessModifier.Size = new System.Drawing.Size(81, 20);
             this.accessModifier.TabIndex = 0;
-            // 
-            // checkStatic
-            // 
-            this.checkStatic.AutoSize = true;
-            this.checkStatic.Location = new System.Drawing.Point(3, 26);
-            this.checkStatic.Name = "checkStatic";
-            this.checkStatic.Size = new System.Drawing.Size(53, 16);
-            this.checkStatic.TabIndex = 1;
-            this.checkStatic.TabStop = true;
-            this.checkStatic.Text = "static";
-            this.checkStatic.UseVisualStyleBackColor = true;
-            // 
-            // checkFinal
-            // 
-            this.checkFinal.AutoSize = true;
-            this.checkFinal.Location = new System.Drawing.Point(62, 26);
-            this.checkFinal.Name = "checkFinal";
-            this.checkFinal.Size = new System.Drawing.Size(46, 16);
-            this.checkFinal.TabIndex = 2;
-            this.checkFinal.TabStop = true;
-            this.checkFinal.Text = "final";
-            this.checkFinal.UseVisualStyleBackColor = true;
             // 
             // type
             // 
             this.type.Location = new System.Drawing.Point(87, 0);
             this.type.Name = "type";
             this.type.Size = new System.Drawing.Size(43, 21);
-            this.type.TabIndex = 3;
+            this.type.TabIndex = 4;
             // 
             // name
             // 
             this.name.Location = new System.Drawing.Point(137, 0);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(73, 21);
-            this.name.TabIndex = 4;
+            this.name.TabIndex = 5;
+            // 
+            // checkStatic
+            // 
+            this.checkStatic.AutoSize = true;
+            this.checkStatic.Location = new System.Drawing.Point(3, 51);
+            this.checkStatic.Name = "checkStatic";
+            this.checkStatic.Size = new System.Drawing.Size(53, 16);
+            this.checkStatic.TabIndex = 6;
+            this.checkStatic.TabStop = true;
+            this.checkStatic.Text = "static";
+            this.checkStatic.UseVisualStyleBackColor = true;
+            // 
+            // checkAbstract
+            // 
+            this.checkAbstract.AutoSize = true;
+            this.checkAbstract.Location = new System.Drawing.Point(62, 51);
+            this.checkAbstract.Name = "checkAbstract";
+            this.checkAbstract.Size = new System.Drawing.Size(68, 16);
+            this.checkAbstract.TabIndex = 7;
+            this.checkAbstract.TabStop = true;
+            this.checkAbstract.Text = "abstract";
+            this.checkAbstract.UseVisualStyleBackColor = true;
             // 
             // create
             // 
-            this.create.Location = new System.Drawing.Point(169, 26);
+            this.create.Location = new System.Drawing.Point(169, 48);
             this.create.Name = "create";
             this.create.Size = new System.Drawing.Size(41, 19);
-            this.create.TabIndex = 5;
+            this.create.TabIndex = 8;
             this.create.Text = "생성";
             this.create.UseVisualStyleBackColor = true;
             this.create.Click += new System.EventHandler(this.create_Click);
             // 
-            // Value_Unit_GUI
+            // param
+            // 
+            this.param.Location = new System.Drawing.Point(0, 26);
+            this.param.Name = "param";
+            this.param.Size = new System.Drawing.Size(210, 21);
+            this.param.TabIndex = 9;
+            // 
+            // MethodUnit_GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.param);
             this.Controls.Add(this.create);
+            this.Controls.Add(this.checkAbstract);
+            this.Controls.Add(this.checkStatic);
             this.Controls.Add(this.name);
             this.Controls.Add(this.type);
-            this.Controls.Add(this.checkFinal);
-            this.Controls.Add(this.checkStatic);
             this.Controls.Add(this.accessModifier);
-            this.Name = "Value_Unit_GUI";
-            this.Size = new System.Drawing.Size(215, 45);
+            this.Name = "MethodUnit_GUI";
+            this.Size = new System.Drawing.Size(215, 70);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,10 +123,11 @@
         #endregion
 
         private System.Windows.Forms.ComboBox accessModifier;
-        private System.Windows.Forms.RadioButton checkStatic;
-        private System.Windows.Forms.RadioButton checkFinal;
         private System.Windows.Forms.TextBox type;
         private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.RadioButton checkStatic;
+        private System.Windows.Forms.RadioButton checkAbstract;
         private System.Windows.Forms.Button create;
+        private System.Windows.Forms.TextBox param;
     }
 }
---
+++ MC2017/MethodUnit_GUI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MC2017
+{
+    public partial class MethodUnit_GUI : UserControl
+    {
+        Unit_Class unit;
+
+        public MethodUnit_GUI(Unit_Class unit)
+        {
+            InitializeComponent();
+            this.unit = unit;
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            string methodType = type.Text;
+            string methodName = name.Text;
+            Unit_Method.access_Modifier methodAccessModifier = new Unit_Method.access_Modifier();
+            bool valStatic = checkStatic.Checked;
+            bool methodAbstract = checkAbstract.Checked;
+            string methodParam = param.Text;
+
+            switch (accessModifier.SelectedIndex)
+            {
+                case 0:
+                    methodAccessModifier = Unit_Method.access_Modifier.PRIVATE;
+                    break;
+                case 1:
+                    methodAccessModifier = Unit_Method.access_Modifier.PUBLIC;
+                    break;
+                case 2:
+                    methodAccessModifier = Unit_Method.access_Modifier.PRIVATE;
+                    break;
+                case 3:
+                    methodAccessModifier = Unit_Method.access_Modifier.PROTECTED;
+                    break;
+            }
+            Unit_Method method = new Unit_Method(methodName, methodType, methodParam, methodAccessModifier, valStatic, methodAbstract);
+            unit.add_Unit_Method(method);
+        }
+    }
+    }
+}
