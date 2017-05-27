597e20b2fbc1e3c8c8ad23efb03972445090307e
HanChangHee <jaduwvad2@gmail.com>
2017-05-27 13:57:06 +0900

    unit_class 생성중 unit_line 생성

---
+++ MC2017/Unit_Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfTest
+{
+    class Unit_Class
+    {
+        private String name;
+        public List<Unit_Line> from;
+        public List<Unit_Line> to;
+
+        public Unit_Class(String name)
+        {
+
+        }
+
+        public void delete_Class()
+        {
+
+        }
+        public void add_Line_From()
+        {
+
+        }
+        public void add_Line_To()
+        {
+
+        }
+    }
+}
