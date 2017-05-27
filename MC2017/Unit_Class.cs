8c047e78b8799885cb14683342425846b88c79df
skdudn321 <skdudn12378@gmail.com>
2017-05-27 14:44:32 +0900

    modify unit_class & unit_line

--- MC2017/Unit_Class.cs
+++ MC2017/Unit_Class.cs
@@ -22,18 +22,29 @@ namespace MC2017
 
         private List<Unit_Value> val;
         private List<Unit_Method> method;
-        public List<Unit_Line> from;
-        public List<Unit_Line> to;
+        private List<Unit_Line> from; // i'm from!
+        private List<Unit_Line> to; // i'm to!
 
-        public Unit_Class(String name, class_Type type)
+        public Unit_Class(String name = "none", class_Type type = class_Type.CLASS)
         {
             this.name = name;
             this.type = type;
+            val = new List<Unit_Value>();
+            method = new List<Unit_Method>();
+            from = new List<Unit_Line>();
+            to = new List<Unit_Line>();
         }
 
         public void delete_Class()
         {
-
+            foreach(var s in from)
+            {
+                s.delete_To();
+            }
+            foreach(var s in to)
+            {
+                s.delete_From();
+            }
         }
         public void add_Line_From(Unit_Line line_From)
         {
@@ -43,13 +54,29 @@ namespace MC2017
         {
             to.Add(line_To);
         }
+        public void delete_Line_From(Unit_Line line_From)
+        {
+            from.Remove(line_From);
+        }
+        public void delete_Line_To(Unit_Line line_To)
+        {
+            to.Remove(line_To);
+        }
         public void add_Unit_Value(Unit_Value val)
         {
             this.val.Add(val);
         }
         public void add_Unit_Method(Unit_Method method)
         {
-            
+            this.method.Add(method);
+        }
+        public void delete_Unit_Value(Unit_Value val)
+        {
+            this.val.Remove(val);
+        }
+        public void delete_Unit_Method(Unit_Method method)
+        {
+            this.method.Remove(method);
         }
     }
 }
