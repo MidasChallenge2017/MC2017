0cd915deff9b9e7becca48a0ab57819770f8cc0c
skdudn321 <skdudn12378@gmail.com>
2017-05-27 19:47:08 +0900

    class private->public / add class_gui

--- MC2017/Unit_Method.cs
+++ MC2017/Unit_Method.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MC2017
 {
-    class Unit_Method
+    public class Unit_Method
     {
         public enum access_Modifier
         {
@@ -15,19 +15,47 @@ namespace MC2017
             PROTECTED
         }
 
-        private string name { get; set; }
-        private string type { get; set; }
-        private access_Modifier modifier { get; set; }
-        private bool static_Modifier { get; set; }
-        private bool abstract_Modifier { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public access_Modifier modifier { get; set; }
+        public string parameter;
+        public bool static_Modifier { get; set; }
+        public bool abstract_Modifier { get; set; }
+        public string str_Print { get; set; }
 
-        public Unit_Method(string get_Name = "none", string get_Type = "none", access_Modifier get_Modifier = access_Modifier.PRIVATE, bool get_Static_Modifier = false, bool get_Abstract_Modifier = false)
+        public Unit_Method(string get_Name = "none", string get_Type = "void", string get_Parameter = "", access_Modifier get_Modifier = access_Modifier.PRIVATE, bool get_Static_Modifier = false, bool get_Abstract_Modifier = false)
         {
             name = get_Name;
             type = get_Type;
             modifier = get_Modifier;
             static_Modifier = get_Static_Modifier;
             abstract_Modifier = get_Abstract_Modifier;
+            parameter = get_Parameter;
+            make_Str_Print();
+        }
+
+        public void make_Str_Print()
+        {
+            str_Print = string.Empty;
+            switch (modifier)
+            {
+                case access_Modifier.PUBLIC:
+                    str_Print += "+";
+                    break;
+                case access_Modifier.PRIVATE:
+                    str_Print += "-";
+                    break;
+                case access_Modifier.PROTECTED:
+                    str_Print += "#";
+                    break;
+            }
+
+            str_Print += "   ";
+            str_Print += name;
+            str_Print += "(";
+            str_Print += parameter;
+            str_Print += ") :";
+            str_Print += type;
         }
     }
 }
